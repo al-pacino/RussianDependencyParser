88f99b44b2eb41b006e3ae0470591e135f0fcb26
Anton Todua <toduaanton@yandex.ru>
2016-05-03 20:06:30 +0400

    refactoring: normalize spaces

--- src/model.h
+++ src/model.h
@@ -24,37 +24,38 @@ typedef unsigned char uchar;
 typedef unsigned int uint;
 
 class Model {
-    public:
-        Model(const char *dictdir);
-        virtual ~Model();
-		bool Save( const string& filename, ostream& out ) const;
-		bool Load( const string& filename, ostream& out );
-		bool Train( const string& filename, ostream& out );
-		double Test( const string& filename, ostream& out );
-		void Print( ostream& out ) const;
-		StringPair Predict( const string& prevTag, const string& curWord );
-		QList<StringPair> GetTags( const string& word, QList<uint> &probs );
+public:
+	Model(const char *dictdir);
+	virtual ~Model();
 
-    private:
-		struct pair_hash {
-			size_t operator () ( const pair<string, string>& p ) const
-			{
-				return hash<string>()( p.first ) ^ hash<string>()( p.second );
-			}
-		};
+	bool Save( const string& filename, ostream& out ) const;
+	bool Load( const string& filename, ostream& out );
+	bool Train( const string& filename, ostream& out );
+	double Test( const string& filename, ostream& out );
+	void Print( ostream& out ) const;
+	StringPair Predict( const string& prevTag, const string& curWord );
+	QList<StringPair> GetTags( const string& word, QList<uint> &probs );
 
-		unordered_map<StringPair, uint, pair_hash> countTagsPair;
-		uint countWords;
+private:
+	struct pair_hash {
+		size_t operator () ( const pair<string, string>& p ) const
+		{
+			return hash<string>()( p.first ) ^ hash<string>()( p.second );
+		}
+	};
 
-        Paradigm *paradigms;
-        string *prefixes;
-        string *suffixes;
-        string *tags;
-        marisa::Trie words;
-        marisa::Trie ends;
+	unordered_map<StringPair, uint, pair_hash> countTagsPair;
+	uint countWords;
 
-		QList<StringPair> getNFandTags( const string& key ) const;
-		vector< pair<string, uint> > getTagsAndCount( const string& key ) const;
+	Paradigm *paradigms;
+	string *prefixes;
+	string *suffixes;
+	string *tags;
+	marisa::Trie words;
+	marisa::Trie ends;
+
+	QList<StringPair> getNFandTags( const string& key ) const;
+	vector< pair<string, uint> > getTagsAndCount( const string& key ) const;
 };
 
 #endif
